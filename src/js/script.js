fcef5ae6455f5da791ed984ee1c98fbe6fb8e802
tanya <tanya16-99@mail.ru>
2022-09-08 16:18:41 +0300

    focus and blur on input and textarea

--- src/js/script.js
+++ src/js/script.js
@@ -19,7 +19,66 @@ document.addEventListener('DOMContentLoaded', () => {
       document.querySelector(`[data-tabs-target="${path}"]`).classList.add('tabs__content_active');
     };
 
+
+
+
+
+
   });
+
+    //плавный скролл
+$( document ).ready(function() {
+    $('a[href^="#"').on('click', function() {
+
+      let href = $(this).attr('href');
+  
+      $('html, body').animate({
+          scrollTop: $(href).offset().top
+      });
+      return false;
+  });
+
+});
+
+    //скрытие placeholder
+/*     const mainForm = document.forms.main;
+    const mainNameInput = mainForm.mainName;
+    const mainTelephoneInput = mainForm.mainTelephone;
+    const mainEmailInput = mainForm.mainEmail;
+
+    let mainNameInputPlaceholder = mainNameInput.placeholder;
+    const constMainNameInputPlaceholder = mainNameInput.placeholder;
+    let mainTelephoneInputPlaceholder = mainTelephoneInput.placeholder;
+    let mainEmailInputPlaceholder = mainEmailInput.placeholder;
+
+    //name
+    mainNameInput.addEventListener('focus', (e) => {
+      mainNameInputPlaceholder = '';
+    });
+  
+    mainNameInput.addEventListener('blur', (e) => {
+        mainNameInput.placeholder = constMainNameInputPlaceholder;
+    });
+
+    //telephone
+    mainTelephoneInput.addEventListener('focus', (e) => {
+      mainTelephoneInputPlaceholder = '';
+    });
+  
+    mainTelephoneInput.addEventListener('blur', (e) => {
+        mainTelephoneInput.placeholder = mainTelephoneInputPlaceholder;
+    });
+
+    //email
+    mainEmailInput.addEventListener('focus', (e) => {
+      mainEmailInputPlaceholder = '';
+    });
+  
+    mainEmailInput.addEventListener('blur', (e) => {
+        mainEmailInput.placeholder = mainEmailInputPlaceholder;
+    });
+ */
+
 
   
 
