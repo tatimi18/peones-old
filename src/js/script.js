bc8eaab98f9c1c1e02d357d86ba462d9ee49f446
tanya <tanya16-99@mail.ru>
2022-09-08 17:33:10 +0300

    arrow pageup

--- src/js/script.js
+++ src/js/script.js
@@ -1,84 +1,49 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const tabs = document.querySelector('.tabs');
-    const tabsBtn = document.querySelectorAll('.tabs__btn');
-    const tabsContent = document.querySelectorAll('.tabs__content');
-  
-    if (tabs) {
-      tabs.addEventListener('click', (e) => {
-        if (e.target.classList.contains('tabs__btn')) {
-          const tabsPath = e.target.dataset.tabsPath;
-          tabsBtn.forEach(el => {el.classList.remove('tabs__btn_active')});
-          document.querySelector(`[data-tabs-path="${tabsPath}"]`).classList.add('tabs__btn_active');
-          tabsHandler(tabsPath);
-        }
-      });
-    }
-  
-    const tabsHandler = (path) => {
-      tabsContent.forEach(el => {el.classList.remove('tabs__content_active')});
-      document.querySelector(`[data-tabs-target="${path}"]`).classList.add('tabs__content_active');
-    };
+		const tabs = document.querySelector('.tabs');
+		const tabsBtn = document.querySelectorAll('.tabs__btn');
+		const tabsContent = document.querySelectorAll('.tabs__content');
+	
+		if (tabs) {
+			tabs.addEventListener('click', (e) => {
+				if (e.target.classList.contains('tabs__btn')) {
+					const tabsPath = e.target.dataset.tabsPath;
+					tabsBtn.forEach(el => {el.classList.remove('tabs__btn_active')});
+					document.querySelector(`[data-tabs-path="${tabsPath}"]`).classList.add('tabs__btn_active');
+					tabsHandler(tabsPath);
+				}
+			});
+		}
+	
+		const tabsHandler = (path) => {
+			tabsContent.forEach(el => {el.classList.remove('tabs__content_active')});
+			document.querySelector(`[data-tabs-target="${path}"]`).classList.add('tabs__content_active');
+		};
+	});
 
-
-
-
-
-
-  });
-
-    //плавный скролл
+		//плавный скролл
 $( document ).ready(function() {
-    $('a[href^="#"').on('click', function() {
+		$('a[href^="#"').on('click', function() {
 
-      let href = $(this).attr('href');
-  
-      $('html, body').animate({
-          scrollTop: $(href).offset().top
-      });
-      return false;
-  });
+			let href = $(this).attr('href');
+	
+			$('html, body').animate({
+					scrollTop: $(href).offset().top
+			});
+			return false;
+	});
 
 });
 
-    //скрытие placeholder
-/*     const mainForm = document.forms.main;
-    const mainNameInput = mainForm.mainName;
-    const mainTelephoneInput = mainForm.mainTelephone;
-    const mainEmailInput = mainForm.mainEmail;
-
-    let mainNameInputPlaceholder = mainNameInput.placeholder;
-    const constMainNameInputPlaceholder = mainNameInput.placeholder;
-    let mainTelephoneInputPlaceholder = mainTelephoneInput.placeholder;
-    let mainEmailInputPlaceholder = mainEmailInput.placeholder;
-
-    //name
-    mainNameInput.addEventListener('focus', (e) => {
-      mainNameInputPlaceholder = '';
-    });
-  
-    mainNameInput.addEventListener('blur', (e) => {
-        mainNameInput.placeholder = constMainNameInputPlaceholder;
-    });
-
-    //telephone
-    mainTelephoneInput.addEventListener('focus', (e) => {
-      mainTelephoneInputPlaceholder = '';
-    });
-  
-    mainTelephoneInput.addEventListener('blur', (e) => {
-        mainTelephoneInput.placeholder = mainTelephoneInputPlaceholder;
-    });
-
-    //email
-    mainEmailInput.addEventListener('focus', (e) => {
-      mainEmailInputPlaceholder = '';
-    });
-  
-    mainEmailInput.addEventListener('blur', (e) => {
-        mainEmailInput.placeholder = mainEmailInputPlaceholder;
-    });
- */
+$(window).scroll(function() {
+	if ($(this).scrollTop() > 1600) {
+		$('.pageup').fadeIn();
+	} else {
+		$('.pageup').fadeOut();
+	}
+});
 
 
-  
+
+
+	
 
